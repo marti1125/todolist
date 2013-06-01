e796c5f1af703495d46c732d06f599baa3573b66
Willy Aguirre <marti1003@hotmail.com>
2013-06-01 17:33:20 -0500

    finish the project

--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"    
   )
 
 
